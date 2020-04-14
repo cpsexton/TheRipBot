026173e4e51ccbb63bae0cec20deaebc013df5c9
Christopher Sexton <cpsexton@gmail.com>
2020-04-14 14:13:24 -0400

    status command now replys with the chosen status

--- commands/status.js
+++ commands/status.js
@@ -4,14 +4,14 @@ bot.commands = new Collection();
 
 module.exports = {
     name: 'status',
-    description: 'changes the bots status',
+    description: 'changes the bots status. options include: online, idle, dnd',
     async execute(message, args, bot) {
         
-        if(!args) { return message.reply(' please add a status. *(ex: $status idle)*')}
+        if(!args) { return message.reply(' please add a status. *(ex: $status idle/online/dnd)*')}
 
         const theStatus = args[0];
 
         await bot.user.setStatus(theStatus);
-        await message.reply('status change successful');
+        await message.reply(` successfully updated status to ${theStatus}`);
     }
 };
